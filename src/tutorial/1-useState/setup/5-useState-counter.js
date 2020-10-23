c0e714d1e72b356172e5bf27f7eeaf77821fcf43
Aman <aman.thakkar@somaiya.edu>
2020-10-23 18:21:00 +0530

    useState and async handling

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const complexIncrease = () => {
+    setTimeout(() => {
+      // setValue(value + 1);
+      setValue((prevState) => {
+        //basically have an other arrow function inside, parameter is prevValue
+        return prevState + 1; //always return something!
+      });
+    }, 2000);
+  };
+
+  //does not work if you click multiple times before 2 seconds are up!!
+  //the setValue function is async
+  return (
+    <React.Fragment>
+      <section
+        style={{ margin: "4rem 0", padding: "2rem 0", border: "2px solid red" }}
+      >
+        <h2>Regular counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          Decrease
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          Reset
+        </button>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          Increase
+        </button>
+      </section>
+      <br />
+      <section
+        style={{ margin: "4rem 0", padding: "2rem 0", border: "2px solid red" }}
+      >
+        <h2>Complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}>
+          Increase later
+        </button>
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default UseStateCounter;
+
+//till now we just did value update
